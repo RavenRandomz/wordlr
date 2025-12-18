7c627da9a13c685416dd5a501d9e03af6188c282
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-12-17 22:25:58 -0600

    test: Verify word validator

--- test/game/five_word_test.cpp
+++ test/game/five_word_test.cpp
@@ -12,4 +12,19 @@ TEST(FiveWord, properlyFormatted_bool)
     EXPECT_FALSE(FiveWord::properlyFormatted(invalid));
     EXPECT_TRUE(FiveWord::properlyFormatted(valid));
 }
+
+TEST(FiveWord, isRealWordleWord)
+{
+    std::string valid{"crane"};
+    std::string valid2{"bites"};
+    std::string valid3{"bytes"};
+    std::string invalid{"crrrr"};
+    std::string invalid2{"byyts"};
+
+    EXPECT_TRUE(FiveWord::isRealWordleWord(valid));
+    EXPECT_TRUE(FiveWord::isRealWordleWord(valid2));
+    EXPECT_TRUE(FiveWord::isRealWordleWord(valid3));
+    EXPECT_FALSE(FiveWord::isRealWordleWord(invalid));
+    EXPECT_FALSE(FiveWord::isRealWordleWord(invalid2));
+}
 }
